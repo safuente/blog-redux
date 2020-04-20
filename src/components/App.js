0dec6c334c5674e225e4b6492e58feb399da5cc5
safuente <santiago.alvarezfuente@telefonica.com>
2020-04-20 16:17:43 +0200

    Basic app template created

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React, { Component} from 'react';
+
+class App extends Component {
+    render() {
+        return <div className="ui container">App</div>
+    } 
+
+}
+
+export default App;
